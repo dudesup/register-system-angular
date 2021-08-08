fe3f225660fec237dd04583852ac63c54390c6de
Daria Supińska <dariasupinska@gmail.com>
2021-08-08 16:21:18 +0200

    getCar and createCarRegistration methods created, home component created

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponseBase } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { stringify } from '@angular/compiler/src/util';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -17,4 +18,13 @@ export class CarService {
     return this.http.get('/server/api/v1/cars');
   }
 
+  getCar(id: number){
+    return this.http.get('/server/api/v1/cars/'+id)
+  }
+
+  createCarRegistration(car){
+    let body = JSON.stringify(car);
+    return this.http.post('/server/api/v1/cars', body, httpOptions);
+  }
+   
 }
